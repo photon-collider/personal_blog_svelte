f5df6fa1d41f19416852975559d6784aa93eb5e2
Bryan Anthonio <dev@bryananthonio.com>
2023-11-13 18:30:00 -0800

    refactor(article-bio): update styling

--- src/lib/components/Article/ArticleBio.svelte
+++ src/lib/components/Article/ArticleBio.svelte
@@ -2,13 +2,11 @@
 	import BioPic from '../BioPic.svelte';
 </script>
 
-<div
-	class="flex flex-col-reverse border-y border-stone-300 py-12 dark:border-stone-700 md:flex-row"
->
-	<div class="flex items-center">
-		<p class="mr-2 inline align-baseline text-base text-stone-800 dark:text-stone-200">
+<div class="flex flex-col-reverse gap-3 text-dark">
+	<div>
+		<p>
 			Hello and thanks for visiting my page! I'm Bryan Anthonio. This is my personal website where I
-			<a href="/blog">write</a> about what I've learned about technology, coding, and life.
+			write about what I've learned about technology, coding, and life.
 		</p>
 	</div>
 	<BioPic />

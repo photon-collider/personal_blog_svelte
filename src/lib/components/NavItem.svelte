e693c5d437c632e98ae4d48759aa4045283f7fb2
Bryan Anthonio <dev@bryananthonio.com>
2023-11-13 18:12:04 -0800

    refactor(nav-item): refactored style to show bottom border when links are active

--- src/lib/components/NavItem.svelte
+++ src/lib/components/NavItem.svelte
@@ -4,11 +4,8 @@
 	export let leftMargin = true;
 </script>
 
-<a
-	class="my-4 text-base text-stone-800 dark:text-stone-200 md:text-lg underline-offset-8 decoration-2
-		{active && 'underline decoration-stone-500/80 dark:decoration-stone-400/80'} 
-		{leftMargin ? 'ml-3 md:ml-5' : 'mr-3 md:mr-5'} "
-	{href}
->
-	<slot />
+<a class="text-darkest" {href}>
+	<div class="py-2 {active && 'border-b-4 border-primary '} ">
+		<slot />
+	</div>
 </a>

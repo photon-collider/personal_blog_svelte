e3d03403ebbf341abacc5c26471f86cf7b522dec
Bryan Anthonio <dev@bryananthonio.com>
2023-11-13 18:28:03 -0800

    refactor(footer): update styling for page footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,21 +6,16 @@
 	let currentYear = DateTime.now().year;
 </script>
 
-<footer class="flex flex-col justify-center bg-stone-200 dark:bg-stone-900">
-	<nav class="container-md py-4">
-		<NavItemGroup items={navFooterItems} leftMargin={false} />
-	</nav>
+<footer class="mt-7 border-t border-light bg-lightest text-darkest">
+	<PageContainer>
+		<nav class="mt-4">
+			<NavItemGroup items={navFooterItems} leftMargin={false} />
+		</nav>
 
-	<div
-		class="container-md py-4 text-sm text-stone-800 dark:text-stone-200 md:text-base"
-		id="copyright"
-	>
-		© 2020-{currentYear}, Bryan Anthonio. All rights reserved. Built using
-		<a class="font-bold text-stone-800 underline dark:text-stone-200" href="https://kit.svelte.dev/"
-			>SvelteKit</a
-		>. Hosted on
-		<a class="font-bold text-stone-800 underline dark:text-stone-200" href="https://www.netlify.com"
-			>Netlify</a
-		>.
-	</div>
+		<div class="py-2" id="copyright">
+			© 2020-{currentYear}, Bryan Anthonio. All rights reserved. Built using
+			<a class="font-bold underline" href="https://kit.svelte.dev/">SvelteKit</a>. Hosted on
+			<a class="font-bold underline" href="https://www.netlify.com">Netlify</a>.
+		</div>
+	</PageContainer>
 </footer>

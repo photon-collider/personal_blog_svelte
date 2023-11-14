b9d71543a7a7ff5f1b39ef7c769f45b542105167
Bryan Anthonio <dev@bryananthonio.com>
2023-11-13 18:17:00 -0800

    refactor(header): update styling for dark mode toggle

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -17,24 +17,13 @@
 		<nav>
 			<NavItemGroup items={headerLinks} />
 		</nav>
-		<button
-			on:click={() => {
-				const isDark = document.documentElement.classList.contains('dark')
-				
-				isDark 
-				? document.documentElement.classList.remove('dark')
-				: document.documentElement.classList.add('dark') 
-
-				window.localStorage.setItem('theme', isDark ? "light" : "dark");
-			}}
-			class="ml-3 inline-block rounded-full border-stone-700  p-1 dark:border-stone-300"
-		>
+			<div class="w-[1.5rem]">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
 				viewBox="0 0 24 24"
 				stroke-width="1.5"
-				class="h-6 w-6 fill-stone-700 stroke-stone-700 dark:fill-stone-200 dark:stroke-stone-200"
+					class="fill-darkest stroke-darkest"
 			>
 				<path
 					stroke-linecap="round"
@@ -42,6 +31,7 @@
 					d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
 				/>
 			</svg>
+			</div>
 		</button>
 	</div>
 </header>

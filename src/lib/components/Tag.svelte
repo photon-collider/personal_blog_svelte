f64beb390e1f7ec2b29d032dea7264d1b6775e96
Bryan Anthonio <dev@bryananthonio.com>
2023-11-17 15:52:36 -0800

    fix(tag): fix bug with URLs

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -5,5 +5,5 @@
 
 <a
 	class={`inline rounded-full border border-light bg-white px-2 py-1 font-mono text-size-0 text-dark hover:border-lightest hover:bg-primary hover:text-primaryActive ${classesToAdd}`}
-	href="/blog/tag/{tag}rounded">{tag}</a
+	href="/blog/tag/{tag}">{tag}</a
 >

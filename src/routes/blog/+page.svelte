e0f71b861ce64a6efe6a5295862424902cd5cd38
Bryan Anthonio <dev@bryananthonio.com>
2023-11-17 13:21:01 -0800

    refactor: add subtitle on blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -9,7 +9,10 @@
 </svelte:head>
 
 <div>
-	<h1>Blog Posts</h1>
-
+	<h1 class="mb-5">Blog Posts</h1>
+	<p class="text-size-2 text-dark">
+		Topics include technology, photography, web development learnings, and musings on life and
+		career
+	</p>
 	<BlogPostsView {articlesGroupedYear} {articleTagData} />
 </div>

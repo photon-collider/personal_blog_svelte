c58b68a2e345a5ca53b47706fc08a9bf73aeebfa
Bryan Anthonio <dev@bryananthonio.com>
2023-11-17 13:09:56 -0800

    refactor: create new component for rendering tags

---
+++ src/lib/components/Tag.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let tag;
+</script>
+
+<a
+	class="inline rounded-full border border-light bg-white px-2 py-1 font-mono text-size-0 text-dark hover:border-lightest hover:bg-primary hover:text-primaryActive"
+	href="/blog/tag/{tag}rounded">{tag}</a
+>

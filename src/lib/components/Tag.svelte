4f09c9b6bb1e251b790beebb95c2b3de7d50a9a1
Bryan Anthonio <dev@bryananthonio.com>
2023-11-17 14:44:58 -0800

    feat(tag): add prop for adding additional classes

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -1,8 +1,9 @@
 <script>
 	export let tag;
+	export let classesToAdd;
 </script>
 
 <a
-	class="inline rounded-full border border-light bg-white px-2 py-1 font-mono text-size-0 text-dark hover:border-lightest hover:bg-primary hover:text-primaryActive"
+	class={`inline rounded-full border border-light bg-white px-2 py-1 font-mono text-size-0 text-dark hover:border-lightest hover:bg-primary hover:text-primaryActive ${classesToAdd}`}
 	href="/blog/tag/{tag}rounded">{tag}</a
 >

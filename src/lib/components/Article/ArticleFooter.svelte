eb19bd6ccc50201d0842aa34e8e377ecd79d867c
Bryan Anthonio <dev@bryananthonio.com>
2023-11-17 15:59:55 -0800

    refactor(article-footer): replace custom css with tailwind classes

--- src/lib/components/Article/ArticleFooter.svelte
+++ src/lib/components/Article/ArticleFooter.svelte
@@ -4,17 +4,17 @@
 	export let tags;
 </script>
 
-<aside class="footer writing">
-	<ArticleBio />
+<div class="footer writing mx-auto my-6">
+	<div class="bg-white p-4 shadow-sm">
+		<ArticleBio />
+	</div>
+
+	<div />
 	<ArticleTagsList {tags} />
-</aside>
+</div>
 
 <style>
 	.footer {
 		max-width: var(--max-width-writing);
-		margin-left: auto;
-		margin-right: auto;
-		margin-top: var(--spacing-xl);
-		margin-bottom: var(--spacing-xl);
 	}
 </style>

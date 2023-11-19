792a06846bd015f2f8e41768b6eb4991675be898
Bryan Anthonio <dev@bryananthonio.com>
2023-11-19 06:57:08 -0800

    refactor(nav-item): reduce letter spacing

--- src/lib/components/NavItem.svelte
+++ src/lib/components/NavItem.svelte
@@ -3,7 +3,7 @@
 	export let active;
 </script>
 
-<a class="text-darkest" {href}>
+<a class="tracking-[-0.014em] text-darkest" {href}>
 	<div class="py-3 {active && 'border-b-4 border-primary '} ">
 		<slot />
 	</div>

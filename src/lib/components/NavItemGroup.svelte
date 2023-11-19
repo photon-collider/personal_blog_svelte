cebb855400e918b6373148ecc35bcf43989c8f60
Bryan Anthonio <dev@bryananthonio.com>
2023-11-19 06:56:28 -0800

    refactor(nav-item): add bigger gap for wider screens

--- src/lib/components/NavItemGroup.svelte
+++ src/lib/components/NavItemGroup.svelte
@@ -4,7 +4,7 @@
 	export let leftMargin = false;
 </script>
 
-<ul class="flex flex-row gap-3">
+<ul class="flex flex-row gap-3 sm:gap-4">
 	{#each items as { href, title, active }}
 		<li>
 			<NavItem {href} {active} {leftMargin}>{title}</NavItem>

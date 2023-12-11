65643bef5f02f73690751b2d7d351d7d2b3b7569
Bryan Anthonio <dev@bryananthonio.com>
2023-12-11 10:52:35 -0800

    fix(css): fixed article content being too wide on mobile screens

--- src/lib/components/Grid2ColContainer.svelte
+++ src/lib/components/Grid2ColContainer.svelte
@@ -2,4 +2,4 @@
 	export let classesToAdd;
 </script>
 
-<div class={`grid lg:grid-cols-[auto_20rem] ${classesToAdd}`}><slot /></div>
+<div class={`grid grid-cols-1 lg:grid-cols-[auto_20rem] ${classesToAdd}`}><slot /></div>

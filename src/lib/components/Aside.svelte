a19bcb64a4512156145aef22c448bd07b9732112
Bryan Anthonio <dev@bryananthonio.com>
2023-12-11 06:22:36 -0800

    refactor(aside): add bottom margin spacing

--- src/lib/components/Aside.svelte
+++ src/lib/components/Aside.svelte
@@ -1,3 +1,5 @@
-<aside class="rounded border-l-4 border-darkest bg-white p-3 text-darkest shadow-sm">
+<aside
+	class="mb-[var(--spacing-3-rem)] rounded border-l-4 border-darkest bg-white p-3 text-darkest shadow-md"
+>
 	<slot />
 </aside>

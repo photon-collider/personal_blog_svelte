33da8ffac81558723d1c1535a74d09b5884cb05b
Bryan Anthonio <50253017+photon-collider@users.noreply.github.com>
2024-01-26 08:01:16 -0800

    Post/stephen king lesson failure (#33)
    * refactor: created new variable in config for contact email
    
    * post: add new post on Stephen King's embrace of failure

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,19 +3,19 @@ export const imgHostRootURL = 'https://banthonio.nyc3.digitaloceanspaces.com';
 
 export const numArticlesToShow = 4;
 export const siteDescription =
-	'Web developer, photographer, and blogger. Welcome to my digital garden!';
+  'Web developer, photographer, and blogger. Welcome to my digital garden!';
 export const siteURL = 'https://bryananthonio.com';
-
+export const contactEmail = 'hey@bryananthonio.com';
 export const navHeaderItems = [
-	{ href: '/', title: 'Home' },
-	{ href: '/blog', title: 'Blog' },
-	{ href: '/projects', title: 'Projects' },
-	{ href: '/about', title: 'About' },
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
 ];
 
 export const navFooterItems = [
-	{ href: '/blog', title: 'Blog' },
-	{ href: '/projects', title: 'Projects' },
-	{ href: '/about', title: 'About' },
-	{ href: '/api/blog-rss.xml', title: 'RSS' }
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+  { href: '/api/blog-rss.xml', title: 'RSS' }
 ];

1a2aa051576efc25241199ca196a3e0c9d1d96c2
Bryan Anthonio <dev@bryananthonio.com>
2024-03-24 12:20:04 -0700

    updated layout for about me page

--- src/routes/about/+layout.svelte
+++ src/routes/about/+layout.svelte
@@ -1,28 +1,28 @@
 <script>
+	import ArticleHeader from '$lib/components/Article/ArticleHeader.svelte';
 	import ArticleBodyText from '$lib/components/Article/ArticleBodyText.svelte';
 </script>
 
-<h1 class="mb-5">About Me</h1>
+<ArticleHeader title="About Me" description="A brief introduction about me and how to reach out." />
 
 <main>
-	<div class="flex flex-col gap-4 sm:flex-row sm:items-center">
-		<div>
-			<p class="text-size-2 text-dark">
-				Hello there, I'm <strong>Bryan Anthonio</strong>. <br /> Welcome to my corner of the internet.
-				👋🏾
-			</p>
-		</div>
-		<div class="">
-			<img
-				class="max-w-[150px] rounded-full"
-				loading="lazy"
-				src="/images/bio_pic.jpg"
-				alt="Portrait of Bryan Anthonio"
-			/>
-		</div>
-	</div>
-
 	<ArticleBodyText>
+		<div class="flex flex-col gap-4 sm:flex-row sm:items-center">
+			<div>
+				<p class="text-size-2 text-dark">
+					Hello there, I'm <strong>Bryan Anthonio</strong>. <br /> Welcome to my corner of the internet.
+					👋🏾
+				</p>
+			</div>
+			<div class="">
+				<img
+					class="max-w-[150px] rounded-full"
+					loading="lazy"
+					src="/images/bio_pic.jpg"
+					alt="Portrait of Bryan Anthonio"
+				/>
+			</div>
+		</div>
 		<slot />
 	</ArticleBodyText>
 </main>

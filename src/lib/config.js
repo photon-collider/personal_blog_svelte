71fb1adae6a267c8e730b09f1cfb5f667378f336
Bryan Anthonio <dev@bryananthonio.com>
2024-04-05 14:17:19 -0700

    add blog post category list to config

--- src/lib/config.js
+++ src/lib/config.js
@@ -8,10 +8,10 @@ export const siteURL = 'https://bryananthonio.com';
 export const contactEmail = 'hey@bryananthonio.com';
 export const twitterHandle = '@beanthonio'
 export const navHeaderItems = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/now', title: 'Now' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/reading', title: 'Reading' },
 ];
 
 export const navFooterItems = [
@@ -20,3 +20,12 @@ export const navFooterItems = [
   { href: '/about', title: 'About' },
   { href: '/api/blog-rss.xml', title: 'RSS' }
 ];
+
+export const blogPostCategories = [
+  { label: "swe", pretty_label: "Software Engineering" },
+  { label: "design", pretty_label: "Design" },
+  { label: "aiml", pretty_label: "AI & Machine Learning" },
+  { label: "personal", pretty_label: "Personal" },
+  { label: "photography", pretty_label: "Photography" },
+  { label: "yearly-review", pretty_label: "Yearly Review" },
+]

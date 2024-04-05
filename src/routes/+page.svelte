e91b0dddbc04b92b830b021c11a0892022892ba2
Bryan Anthonio <dev@bryananthonio.com>
2024-04-05 14:46:33 -0700

    started revising home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import HeroBanner from '$lib/components/HeroBanner.svelte';
+	import BioPic from '$lib/components/BioPic.svelte';
 </script>
 
 <svelte:head>
@@ -11,4 +12,20 @@
 	/>
 </svelte:head>
 
-<HeroBanner />
+<div class="relative mx-auto max-w-[720px]">
+	<h1 class="text-[2rem] font-normal tracking-wide">
+		Hi! I’m <span class="font-bold">Bryan</span>.
+	</h1>
+
+	<BioPic wrapperClass="absolute left-[-120px] top-[-5%]" imgClass="rounded-full w-[96px]" />
+
+	<p class="mt-3 max-w-[526px] text-[1.125rem]">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris nisi ut aliquip ex ea commodo consequat.
+	</p>
+
+	<h2 class="text-[1.5rem]">What I’m Doing Now</h2>
+
+	<h2 class="text-[1.5rem]">Recent Blog Posts</h2>
+</div>

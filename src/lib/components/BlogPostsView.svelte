e22b33accc96680bcb74fc9182c212117b288286
Bryan Anthonio <dev@bryananthonio.com>
2024-04-05 14:16:45 -0700

    refactor: group blog posts by category instead of by year

--- src/lib/components/BlogPostsView.svelte
+++ src/lib/components/BlogPostsView.svelte
@@ -1,26 +1,20 @@
 <script>
-	import Grid2ColContainer from './Grid2ColContainer.svelte';
 	import ArticleList from './ArticleList.svelte';
-	import ArticleTagsList from './ArticleTagsList.svelte';
-	export let articleTagData;
-	export let articlesGroupedYear;
+	export let articlesGroupedCategory;
+	import { blogPostCategories } from '$lib/config';
+	console.log(blogPostCategories);
 
-	const articleTags = Object.keys(articleTagData);
-	const articleYears = Object.keys(articlesGroupedYear).reverse();
+	let temp = articlesGroupedCategory[blogPostCategories[0]['label']];
+	console.log(blogPostCategories.length);
 </script>
 
-<Grid2ColContainer classesToAdd="lg:gap-7">
-	<main>
-		{#each articleYears as year}
-			<div class="mb-7">
-				<h2 class="mb-3">{year}</h2>
-				<ArticleList articleList={articlesGroupedYear[year]} />
-			</div>
-		{/each}
-	</main>
-	<aside>
-		<div class="rounded border border-light p-3 shadow-sm lg:mt-[var(--spacing-6-rem)]">
-			<ArticleTagsList tags={articleTags} titleTop={true} />
+<main>
+	{#each blogPostCategories as categoryObj}
+		<div class="mb-7">
+			<h2 class="mb-4 mt-[88px] border-light !text-size-1">
+				{categoryObj['pretty_label']}
+			</h2>
+			<ArticleList articleList={articlesGroupedCategory[categoryObj['label']]} />
 		</div>
-	</aside>
-</Grid2ColContainer>
+	{/each}
+</main>
